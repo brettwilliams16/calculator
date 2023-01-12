80a1c8871b35007d2d0dd8eadfaa162ff24c2327
brettwilliams16 <brett3901@gmail.com>
2023-01-12 17:26:16 -0500

    added functionality to clear, delete, and operand buttons

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 const bottomDisplay = document.querySelector(".bottom-display");
 const topDisplay = document.querySelector(".top-display");
-
-
+const clear = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".delete");
 const numbers = document.querySelectorAll(".number");
+const operands = document.querySelectorAll(".operand");
+
+let numberOne = 0; // first number to be saved
+let numberTwo = 0; // second number to be saved
+
+
+
+
+operands.forEach(operand => {
+    operand.addEventListener('click', () =>{
+        if(isNaN(topDisplay.textContent.charAt(topDisplay.textContent.length - 1))){
+            return;
+        }
+        numberOne = topDisplay.textContent;
+        topDisplay.textContent += ` ${operand.textContent} `;
+    })
+})
 
 numbers.forEach(number => {
     number.addEventListener('click', () =>{
@@ -10,3 +27,24 @@ numbers.forEach(number => {
     })
 });
 
+clear.addEventListener('click', clearDisplay);
+deleteBtn.addEventListener('click', deleteDigit);
+
+
+
+
+
+
+function clearDisplay() {
+    topDisplay.textContent = '';
+}
+
+function deleteDigit() {
+    if(topDisplay.textContent.charAt(topDisplay.textContent.length -1) === ' '){
+        topDisplay.textContent = topDisplay.textContent.slice(0, -2);
+    }
+    else {
+        topDisplay.textContent = topDisplay.textContent.slice(0, -1);
+    }
+    
+}
